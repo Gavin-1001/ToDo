2949aae52007b90bd9dacc3dc802ae37e88ee763
Gavin-1001 <gavinshelley10@gmail.com>
2023-02-04 22:29:49 +0000

    added task-item component and task component

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {Task} from "../../model/Task";
+import {TASKS} from "../../mock-task";
 
 @Component({
   selector: 'app-tasks',
@@ -6,5 +8,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./tasks.component.scss']
 })
 export class TasksComponent {
+
+    tasks: Task[] = TASKS;
 
 }
