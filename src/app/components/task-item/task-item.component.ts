9ccc74d1d1d191b6529c7e2d69acd816e4f37fad
Gavin-1001 <gavinshelley10@gmail.com>
2023-02-13 00:31:12 +0000

    Added Task-Item component, interpolation of each task

--- src/app/components/task-item/task-item.component.ts
+++ src/app/components/task-item/task-item.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input } from '@angular/core';
 import {Task} from "../../model/Task";
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+
 
 @Component({
   selector: 'app-task-item',
@@ -10,5 +12,6 @@ export class TaskItemComponent {
 
     @Input() task: Task;
 
+    faTimes = faTimes;
 
 }
